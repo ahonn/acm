6462ec42d4905ce16466ca8d4713575e87d9f18f
ahonn <ahonn95@outlook.com>
2016-01-16 02:26:31 +0800

    1 Two Sum

---
+++ 1.Two_Sum.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Given an array of integers, find two numbers such that they add up to a specific target number.
+
+# The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+# You may assume that each input would have exactly one solution.
+
+# Input: numbers={2, 7, 11, 15}, target=9
+# Output: index1=1, index2=2
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        length = len(nums)
+        map = {}
+        for i in xrange(length):
+            x = nums[i]
+            if target-x in map:
+                return (map[target-x]+1, i+1)
+            map[x] = i
