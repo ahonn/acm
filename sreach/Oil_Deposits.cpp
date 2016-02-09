78445f604b4e012361236bc50cabfa486ff74a69
ahonn <ahonn95@outlook.com>
2016-02-09 15:12:56 +0800

    Update

--- sreach/Oil_Deposits.cpp
+++ sreach/Oil_Deposits.cpp
@@ -1,9 +1,35 @@
-/* 
-* @Author: ahonn
-* @Date:   2016-02-07 21:33:18
-* @Last Modified by:   ahonn
-* @Last Modified time: 2016-02-09 14:13:23
-*/
+// Oil Deposits
+
+// Description
+// The GeoSurvComp geologic survey company is responsible for detecting underground oil deposits. GeoSurvComp works with one large rectangular region of land at a time, and creates a grid that divides the land into numerous square plots. It then analyzes each plot separately, using sensing equipment to determine whether or not the plot contains oil. A plot containing oil is called a pocket. If two pockets are adjacent, then they are part of the same oil deposit. Oil deposits can be quite large and may contain numerous pockets. Your job is to determine how many different oil deposits are contained in a grid.
+// Input
+// The input contains one or more grids. Each grid begins with a line containing m and n, the number of rows and columns in the grid, separated by a single space. If m = 0 it signals the end of the input; otherwise 1 <= m <= 100 and 1 <= n <= 100. Following this are m lines of n characters each (not counting the end-of-line characters). Each character corresponds to one plot, and is either `*', representing the absence of oil, or `@', representing an oil pocket. 
+
+// Output
+// are adjacent horizontally, vertically, or diagonally. An oil deposit will not contain more than 100 pockets.
+
+// Sample Input
+// 1 1
+// *
+// 3 5
+// *@*@*
+// **@**
+// *@*@*
+// 1 8
+// @@****@*
+// 5 5 
+// ****@
+// *@@*@
+// *@**@
+// @@@*@
+// @@**@
+// 0 0
+
+// Sample Output
+// 0
+// 1
+// 2
+// 2
 
 #include <iostream>
 #include <cstdio>
@@ -37,7 +63,7 @@ void DFS(int i, int j) {
 }
 
 int main(){
-    freopen("../input.txt", "rt", stdin);
+    // freopen("../input.txt", "rt", stdin);
     int i, j;
     while(scanf("%d %d", &n, &m)) {
         if( ! n && ! m) {
